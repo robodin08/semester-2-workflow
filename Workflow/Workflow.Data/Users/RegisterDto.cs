a778a91f16019dabd1973186fe100c87a2536eb4
Dinand <581569@student.fontys.nl>
2026-05-22 10:43:33 +0200

    Fix merge conflict markers in DTO

--- Workflow/Workflow.Data/Users/RegisterDto.cs
+++ Workflow/Workflow.Data/Users/RegisterDto.cs
@@ -1,10 +1,6 @@
 ﻿namespace Data.Users;
 
-<<<<<<<< HEAD:Workflow/Workflow.Data/Users/CreateUserDto.cs
-public class CreateUserDto(string email, string username, string passwordHash)
-========
 public class RegisterDto(string email, string username, string passwordHash)
->>>>>>>> origin/User-Login:Workflow/Workflow.Data/Users/RegisterDto.cs
 {
     public string Email { get; } = email;
     public string Username { get; } = username;
